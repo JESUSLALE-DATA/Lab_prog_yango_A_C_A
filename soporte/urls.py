5bd6b311a549025bf94c79aafb37dd59e2a3845f
AMUEDOaaron <aaronamuedo2000@gmail.com>
2026-08-25 21:34:22 -0300

    Agrega edicion y eliminacion de reparaciones

--- soporte/urls.py
+++ soporte/urls.py
@@ -3,5 +3,22 @@ from . import views
 
 urlpatterns = [
     path('tecnicos/', views.lista_tecnicos, name='lista_tecnicos'),
-    path('reparaciones/', views.lista_reparaciones, name='lista_reparaciones'),
+
+    path(
+        'reparaciones/',
+        views.lista_reparaciones,
+        name='lista_reparaciones'
+    ),
+
+    path(
+        'reparaciones/eliminar/<int:id>/',
+        views.eliminar_reparacion,
+        name='eliminar_reparacion'
+    ),
+
+    path(
+        'reparaciones/editar/<int:id>/',
+        views.editar_reparacion,
+        name='editar_reparacion'
+    ),
 ]
--- soporte/views.py
+++ soporte/views.py
@@ -22,3 +22,19 @@ def eliminar_reparacion(request, id):
     reparacion = get_object_or_404(Reparacion, id=id)
     reparacion.delete()
     return redirect('lista_reparaciones')
+def editar_reparacion(request, id):
+    reparacion = get_object_or_404(Reparacion, id=id)
+
+    if request.method == 'POST':
+        reparacion.diagnostico = request.POST.get('diagnostico')
+        reparacion.fecha_entrega = request.POST.get('fecha_entrega')
+        reparacion.estado = request.POST.get('estado')
+        reparacion.solucion = request.POST.get('solucion')
+
+        reparacion.save()
+
+        return redirect('lista_reparaciones')
+
+    return render(request, 'soporte/editar_reparacion.html', {
+        'reparacion': reparacion
+    })
